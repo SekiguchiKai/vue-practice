e2f563b7b2f251c025dc1109cfd0b2d309d58487
sekky0905 <ks.sekiguchi.0905@gmail.com>
2018-10-23 19:32:24 +0900

    SFCの簡単なサンプルを作成した

---
+++ sfc/hello_world.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="message">{{msg}}</p>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {msg: 'hello Vue!'};
+        }
+    }
+</script>
+
+<style scoped>
+    .message{color: aqua}
+</style>
